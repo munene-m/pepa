e4d6d4054ff6346823abf592a8c20fef00a771e1
munene-m <munenenjue18@gmail.com>
2025-01-23 14:50:15 +0300

    chore: remove some fields

--- internal/models/user.go
+++ internal/models/user.go
@@ -8,12 +8,8 @@ type User struct {
 	ID            uint      `gorm:"primaryKey;autoIncrement" json:"id"`
 	Email         string    `gorm:"unique;not null;size:255" json:"email"`
 	Name          string    `gorm:"size:255" json:"name"`
-	FirstName     string    `gorm:"size:100" json:"first_name"`
-	LastName      string    `gorm:"size:100" json:"last_name"`
 	
 	GoogleID      string    `gorm:"unique;size:255" json:"google_id"`
-	AccessToken   string    `gorm:"type:text" json:"-"` // Omitted from JSON for security
-	RefreshToken  string    `gorm:"type:text" json:"-"` // Omitted from JSON for security
 	
 	ProfilePicture string    `gorm:"type:text" json:"profile_picture"`
 	Locale         string    `gorm:"size:10" json:"locale"`
